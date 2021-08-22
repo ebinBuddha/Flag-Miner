d64f83894e5c1a2733295c68867e1f277a42e051
ebinBuddha <30810167+ebinBuddha@users.noreply.github.com>
2021-08-22 19:11:14 +0200

    remove unneeded includes

--- FlagMiner/OptionsForm.cs
+++ FlagMiner/OptionsForm.cs
@@ -1,14 +1,7 @@
-using Microsoft.VisualBasic;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.Drawing;
-using System.Diagnostics;
 using System.Windows.Forms;
 using System.Linq;
-using System.Xml.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel;
 using System.IO;
 namespace FlagMiner
